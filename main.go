57548e5c19db374d85c8ee6382dae6b670a74757
Gabriel Diez <diez.gabriel64@gmail.com>
2018-05-05 11:50:47 +0200

    add readCSV

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"encoding/csv"
 	"fmt"
+	"log"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
-	fmt.Printf("Hello World")
+	l, err := readCSV()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	spew.Dump(l)
+}
+
+func readCSV() ([][]string, error) {
+	csvFile := flag.String("csv", "problems.csv", "Csv file in 'question, answer' format")
+	flag.Parse()
+
+	file, err := os.Open(*csvFile)
+
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't open file")
+	}
+
+	r := csv.NewReader(file)
+
+	l, err := r.ReadAll()
+
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't read file")
+	}
+
+	return l, nil
 }
