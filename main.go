70d79bb9adfee8579b669f5267c12268ea96b573
Gabriel Diez <diez.gabriel64@gmail.com>
2018-05-05 17:08:12 +0200

    add timer

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"time"
 )
 
 type quiz struct {
@@ -14,36 +15,47 @@ type quiz struct {
 }
 
 func main() {
-	l, err := readCSV()
+	csvFile := flag.String("csv", "problems.csv", "Csv file in 'question, answer' format")
+	sec := flag.Int("time", 30, "Time in second for answer the quiz")
+	flag.Parse()
+
+	l, err := readCSV(*csvFile)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	quiz := buildQuiz(l)
-
 	score := 0
+	timer := time.NewTimer(time.Duration(*sec) * time.Second)
 
-	for i, q := range quiz {
-		fmt.Printf("Question %d: %s ?\n", i + 1, q.question)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
+	quizLoop:
+		for i, q := range quiz {
+			fmt.Printf("Question %d: %s ?\n", i + 1, q.question)
 
-		if answer == q.solution {
-			score++
+			answerC := make(chan string)
+
+			go func() {
+				var answer string
+				fmt.Scanf("%s\n", &answer)
+				answerC <- answer
+			}()
+
+			select {
+			case <- timer.C:
+				fmt.Printf("Time out !\n")
+				break quizLoop;
+			case answer := <-answerC:
+				if answer == q.solution {
+					score++
+				}
+			}
 		}
-	}
-
 	fmt.Printf("Your score is %d/%d.\n", score, len(quiz))
-
 }
 
-func readCSV() (lines [][]string, err error) {
-	csvFile := flag.String("csv", "problems.csv", "Csv file in 'question, answer' format")
-	flag.Parse()
-
-	file, err := os.Open(*csvFile)
-
+func readCSV(csvFile string) (lines [][]string, err error) {
+	file, err := os.Open(csvFile)
 	if err != nil {
 		return nil, fmt.Errorf("Couldn't open file")
 	}
@@ -51,9 +63,7 @@ func readCSV() (lines [][]string, err error) {
 	defer file.Close()
 
 	r := csv.NewReader(file)
-
 	l, err := r.ReadAll()
-
 	if err != nil {
 		return nil, fmt.Errorf("Couldn't read file")
 	}
@@ -63,7 +73,7 @@ func readCSV() (lines [][]string, err error) {
 
 func buildQuiz(lines [][]string) []quiz {
 	q := make([]quiz, len(lines))
-
+	
 	for i, l := range lines {
 		q[i] = quiz {
 			question: l[0],
